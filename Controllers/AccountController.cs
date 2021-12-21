3467b3ab408a1e59c43225c4d5c24cd7750b607b
Fhaidy Maciel <fhaidy@outlook.com>
2021-12-20 22:31:25 -0300

    added authorization

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,6 +4,8 @@ using Blog.Models;
 using Blog.Services;
 using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
 
 namespace Blog.Controllers
 {
@@ -14,10 +16,43 @@ namespace Blog.Controllers
         public AccountController(TokenService tokenService) => _tokenService = tokenService;
 
         [HttpPost("v1/accounts/login")]
-        public IActionResult Login() => Ok(_tokenService.GenerateToken(null));
+        public async Task<IActionResult> Login(
+            [FromBody] LoginViewModel model,
+            [FromServices] BlogDataContext context)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+            var user = await context
+                .Users
+                .AsNoTracking()
+                .Include(x => x.Roles)
+                .FirstOrDefaultAsync(user => user.Email == model.Email);
+
+            if (user == null)
+                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválido"));
+
+            if (!PasswordHasher.Verify(user.PasswordHash, model.Password))
+                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválido"));
+
+            try
+            {
+                var token = _tokenService.GenerateToken(user);
+                return Ok(new ResultViewModel<string>(token, errors: null));
+            }
+            catch
+            {
+                return StatusCode(
+                    500,
+                    new ResultViewModel<string>("Falha Interna de Servidor")
+                );
+            }
+        }
 
         [HttpPost("v1/accounts/")]
-        public async Task<IActionResult> Post([FromBody] RegisterViewModel model, [FromServices] BlogDataContext context)
+        public async Task<IActionResult> Post(
+            [FromBody] RegisterViewModel model, 
+            [FromServices] BlogDataContext context)
         {
             if (!ModelState.IsValid)
                 return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
@@ -29,7 +64,33 @@ namespace Blog.Controllers
                 Slug = model.Email.Replace("@", "-").Replace(".","-")
             };
 
-            return Ok(new ResultViewModel<User>(user));
+            var password = PasswordGenerator.Generate(25);
+            user.PasswordHash = PasswordHasher.Hash(password);
+
+            try
+            {
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    user = user.Email,
+                    password,
+                }));
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(
+                    500,
+                    new ResultViewModel<string>("Usuário já está cadastrado.")
+                );
+            }
+            catch
+            {
+                return StatusCode(
+                    500,
+                    new ResultViewModel<string>("Falha Interna de Servidor")
+                );
+            }
         }
 
 
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Models;
+﻿using Blog.Extensions;
+using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -12,21 +13,15 @@ namespace Blog.Services
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+            var claims = user.GetClaims();
             var tokenDescriptor = new SecurityTokenDescriptor
             {
+                Subject = new ClaimsIdentity(claims), 
                 Expires = DateTime.UtcNow.AddHours(8),
                 SigningCredentials = new SigningCredentials(
                     new SymmetricSecurityKey(key),
                     SecurityAlgorithms.HmacSha256
                 ),
-                Subject = new ClaimsIdentity( new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, "fhaidy"),
-                    new Claim(ClaimTypes.Role, "user"),
-                    new Claim(ClaimTypes.Role, "author"),
-                    new Claim(ClaimTypes.Role, "admin"),
-                    new Claim("fruta", "banana")
-                })
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
